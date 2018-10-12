cfee45bdcc78521461533b61d591e1b6603b332a
BuildTools <unconfigured@null.spigotmc.org>
2018-10-12 19:24:52 -0400

    First commit to github.

---
+++ Simple Tests/src/simpleTests/SimpleTest.java
@@ -0,0 +1,16 @@
+package simpleTests;
+
+public class SimpleTest {
+
+	public static void main(String[] args) {
+		 int age = 43;
+		   if (age == 43){
+		     System.out.print("Bob is 43 ");
+		   }
+		   if (age == 50){
+		     System.out.print("Bob is 50 "); 
+		   }
+
+	}
+
+}
