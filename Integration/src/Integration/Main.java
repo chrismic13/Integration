66cc0e276de785c5bd5b5e0d0007622fd6080e5c
Christian <christian.mccann1@gmail.com>
2018-10-12 21:54:41 -0400

    Initial Push

---
+++ Integration/src/Integration/Main.java
@@ -0,0 +1,28 @@
+package Integration;
+//Christian McCann
+//This project is a collection of all the knowledge learned this semester in java.
+/*byte - an 8-bit signed two's complement integer. It has a minimum value of -128 and a maximum value of 127 (inclusive)
+ *short -  16-bit signed two's complement integer. It has a minimum value of -32,768 and a maximum value of 32,767 (inclusive)
+ *int - a 32-bit signed two's complement integer, which has a minimum value of -231 and a maximum value of 231-1.
+ *long - a 64-bit two's complement integer. The signed long has a minimum value of -263 and a maximum value of 263-1
+ *float - a single-precision 32-bit IEEE 754 floating point. Has a wide range of values
+ *double - a double-precision 64-bit IEEE 754 floating point. Has a wide range of values
+ * boolean - has only two possible values: true and false
+ * char - a single 16-bit Unicode character. It has a minimum value of '\u0000' (or 0) and a maximum value of '\uffff' (or 65,535 inclusive).
+ * String - not a data type. a collection of characters.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to my Integration Project");
+		int a = 42;
+		double b = 42.5;
+		boolean c = false;
+		String d = "42 is a number";
+		final int fin = 4;
+		//the final keyword makes it so that the value of the variable can not be changed.
+		
+	}
+
+}
